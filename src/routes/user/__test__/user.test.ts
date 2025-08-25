8f79bf261c56c66575224084de6a75817eea575d
Brady Adger <brady.adger12@gmail.com>
2025-08-25 15:08:39 -0600

    fix ts bugs

--- src/routes/user/__test__/user.test.ts
+++ src/routes/user/__test__/user.test.ts
@@ -2,7 +2,6 @@ import { test, teardown } from 'tap'
 import buildServer from '../../../server'
 import { registerAndLoginUser } from '../../../lib/helper'
 import { prisma } from '../../../db'
-import { postMatchupCron } from '../../../cron/post_matchup'
 
 test('GET /user/me - fetch own user data', async (t) => {
     const fastify = buildServer()
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,8 +1,6 @@
 import { teardown } from "tap";
-import { postMatchupCron } from "../cron/post_matchup";
 import { prisma } from "../db";
 
 teardown(async () => {
     await prisma.user.deleteMany({})
-    await prisma.squad.deleteMany({})
 });
